b738c0f043240052874dd69fef6ba35cf059157e
Vitaly Mayatnikov <mayatnikov@gmail.com>
2019-10-20 19:38:02 +0300

    rename it

--- src/main/java/mapper/converters/ClassAtoClassBMapper.java
+++ src/main/java/mapper/converters/ClassAtoClassBMapper.java
@@ -8,29 +8,18 @@
 package mapper.converters;
 
 import ma.glasnost.orika.CustomMapper;
-import ma.glasnost.orika.MapperFactory;
-import mapper.datamodel.ResoRequest;
-import mapper.datamodel.UwCache;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import mapper.datamodel.ClassB;
+import mapper.datamodel.ClassA;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
-public class Mapper1 {
-    static final Logger log = LogManager.getLogger(Mapper1.class);
+public class ClassAtoClassBMapper extends AbstractMapper{
 
-    @Autowired
-    MapperFactory mapperFactory;
-
-    @PostConstruct
     public void  init() {
  // --------------------------------
  // Это настройка и регистрация преобразователя класса A в класс B
-        CustomMapper customMapper = new Cache2ResoCustomMapper();               // класс - нестандартные правила преобразования полей
-        mapperFactory.classMap(UwCache.class, ResoRequest.class)                     // преобразователь для A --> B
+        CustomMapper customMapper = new ClassAtoClassBCustomMapper();               // класс - нестандартные правила преобразования полей
+        mapperFactory.classMap(ClassA.class, ClassB.class)                     // преобразователь для A --> B
                 .field("data['MAIN_USAGE_REGION_KLADR']","carOwnerAddrKladr")      // простейший мапер поля
                 .field("data['USER_FULLNAME']","userName")
                 .customize(customMapper)                                              // вызов всех нестандартных преобразвоаний
